52a8ac4f6926a20a02ec332d7cc35c3f06eccba8
Jasson-AXE <97408959+Jasson-AXE@users.noreply.github.com>
2023-12-13 17:26:55 +0100

    Update jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -2,6 +2,7 @@ pipeline {
     agent any
     parameters {
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS', defaultValue: 'linux')
+        choice(name: 'ARCH', choices: ['x86', 'x64', 'arm'], description: 'Pick architecture', defaultValue: 'x64')
     }
     stages {
         stage('Example') {
